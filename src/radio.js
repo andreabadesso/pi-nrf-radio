9c18501889f379b8671783cc05bf1f2590796c40
Andre Abadesso <andre.abadesso@gmail.com>
2015-11-21 18:21:13 -0200

    feat(radio): added method for executing commands on the radio via spi

--- src/radio.js
+++ src/radio.js
@@ -21,7 +21,55 @@ class Radio {
         this.irq = GPIO.connect(pins.IRQ);
     }
 
+    /*
+     * Blocks the thread for x ms
+     *
+     * @param {Number} block Time in microseconds to block the thread
+     */
+    block(us) {
+        let start = process.hrtime();
+        while(1) {
+            let diff = process.hrtime(start);
+            if (diff[0] * 1e9 + diff[1] >= us * 1e3) break;
+        }
+
+        console.log('Blocked for ' + us + ' us');
+    }
+
+    /*
+     * Changes the state of the CE pin
+     *
+     * @param {String} state State to set the CE Pin ('high' or 'low')
+     * @param {Boolean} block Time in microseconds to block the thread
+     */
     setCE(state, block) {
-        ce.mode(state);
+        this.ce.mode(state);
+        if (block) {
+            this.block(block);
+        }
+    }
+
+    /*
+     * Executes a command on the radio via spi, returning its response
+     *
+     * @param {Number} cmd The command to execute on the radio
+     * @param {Array} data Array of bytes with data to execute on the radio
+     * @param {Function} cb The callback that will be executed on the spi response
+     */
+    execCommand(cmd, data, cb) {
+        let writeBuf,
+            readLen = 0;
+
+        data.reverse();
+        writeBuf = Buffer([cmd].concat(data));
+        data.reverse();
+
+        spi.transfer(writeBuf, readLen && readLen + 1, (err, data) => {
+            if (err) {
+                return cb(err);
+            }
+
+            return cb(null, data && Array.prototype.reverse.call(data.slice(1)));
+        });
     }
 }
