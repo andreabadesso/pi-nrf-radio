feb38c3341523232f2f37bb3be1cc53d4f5e0361
Andre Abadesso <andre.abadesso@gmail.com>
2015-11-21 18:48:16 -0200

    fix(radio): fixed typo on spi

--- src/radio.js
+++ src/radio.js
@@ -89,7 +89,7 @@ class Radio {
         writeBuf = Buffer([cmd].concat(data));
         data.reverse();
 
-        this.SPI.transfer(writeBuf, readLen && readLen + 1, (err, data) => {
+        this.spi.transfer(writeBuf, readLen && readLen + 1, (err, data) => {
             if (err) {
                 return cb(err);
             }
